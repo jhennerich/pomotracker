12597dab3a835300186ef46ecd16bf0111726b9c
John Hennerich <john.hennerich@gmail.com>
2023-10-30 12:42:28 -0600

    Inital commit

---
+++ src/test/java/org/frb/kc/pomotracker/PomotrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.frb.kc.pomotracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomotrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
